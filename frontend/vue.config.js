8e79de2c1283accaf2881677abec128f772b4530
Samuel Iatarola <samueliatarola@gmail.com>
2023-05-29 12:20:13 -0300

    [FEAT] Created base scss files on project.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/base/sass/_core.scss";`
+      }
+    }
+  }
+}
