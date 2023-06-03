199780b6b99ecf71fef0b4458febeaed638a4a34
Heitor Vidal <heitorbvidal@outlook.com>
2023-06-03 01:02:10 -0300

    [FEAT] Login Update & Loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <main>
+    <div v-if="loading" class="loading-screen">
+      <img src="./assets/logoMaxQuality.jpg" alt="Carregando..." class="spinner" />
+    </div>
+    <main v-else>
       <router-view></router-view>
       <ThemeSwitch/>
     </main>
@@ -8,7 +11,7 @@
 </template>
 
 <script>
-import { inject } from 'vue'
+import { inject, ref } from 'vue'
 import ThemeSwitch from './components/theme-swich/vue/ThemeSwich.vue'
 
 export default {
@@ -19,12 +22,40 @@ export default {
   setup() {
     const theme = inject('theme')
 
+    const loading = ref(true)
+
+    setTimeout(() => {
+      loading.value = false
+    }, 2000)
+
     return {
-      theme
+      theme,
+      loading
     }
   },
 }
 </script>
 
 <style>
+.loading-screen {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.spinner {
+  animation: spin 4s linear infinite;
+  max-width: 50px;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 </style>
